74091b7a9518fc2a29b61550d26aa8eb885e8275
amy14rubio <amy14rubio@gmail.com>
2025-11-03 09:53:35 -0500

    finished problem 6

--- from-scratch.js
+++ from-scratch.js
@@ -49,7 +49,11 @@ const sampleUsers = [
 ];
 
 // Problem 6
-const printUserNamesAndScores = (users) => {};
+const printUserNamesAndScores = (users) =>
+  users.forEach((user) =>
+    console.log(`${user.name} scored ${user.score} points`)
+  );
+printUserNamesAndScores(sampleUsers);
 
 // Problem 7
 const getUserNames = (users) => {};
